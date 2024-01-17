806c1078d07dda9ccaa9f8e0f125a64bc12e2d4d
Pritam Singh <iamcreativepritam@gmail.com>
2024-01-17 10:55:40 +0530

    UI is final

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 let menuBtn = document.querySelector("#menu-btn");
+let logoBtn = document.querySelector(".logo");
 let navbarr = document.querySelector(".header .flex .navbar");
+let navEle = document.querySelectorAll(".header .flex .navbar .nav-ele");
+
+console.log(navEle);
 
 menuBtn.onclick = () => {
   menuBtn.classList.toggle("fa-times");
   navbarr.classList.toggle("active");
 };
+
+logoBtn.addEventListener("click", () => {
+  navEle.forEach((nav) => {
+    nav.classList.remove("active");
+  });
+  document.querySelector(".home").classList.add("active");
+});
+
+navEle.forEach((singleNav, i) => {
+  singleNav.addEventListener("click", (e) => {
+    navEle.forEach((nav) => {
+      nav.classList.remove("active");
+      console.log(nav);
+    });
+    e.target.classList.add("active");
+  });
+  console.log(singleNav.classList.contains("active"));
+});
 
 var swiper = new Swiper(".course-slider", {
   spaceBetween: 20,
@@ -28,43 +50,43 @@ var swiper = new Swiper(".course-slider", {
 });
 
 var swiper = new Swiper(".teachers-slider", {
-    spaceBetween: 20,
-    grabCursor: true,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
     },
-    breakpoints: {
-      540: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      991: {
-        slidesPerView: 3,
-      },
+    768: {
+      slidesPerView: 2,
     },
-  });
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
 
 var swiper = new Swiper(".reviews-slider", {
-    spaceBetween: 20,
-    grabCursor: true,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
     },
-    breakpoints: {
-      540: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      991: {
-        slidesPerView: 3,
-      },
+    768: {
+      slidesPerView: 2,
     },
-  });
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
